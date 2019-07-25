6072176c742df512f5efec7601814993cb178c08
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-25 22:41:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  result = {}
+
+  color = data[:color]
+  gender = data[:gender]
+  lives = data[:lives]
+  gender.each do |k, v|
+    v.each do |name|
+      result[name] = {:color => [],
+      :gender => [],
+      :lives => []
+      }
+    end
+  end
+  
+  data.each do |key, info|
+    info.each do |value, names|
+      names.each do |name|
+        result[name][key].push(value.to_s)
+      end
+    end
+  end
+  
+  return result
+end 
